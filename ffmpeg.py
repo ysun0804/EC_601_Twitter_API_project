5f69b5d25a1194be9b091024c9b68f796e43f94c
ysun0804 <43080446+ysun0804@users.noreply.github.com>
2018-09-19 13:02:33 -0400

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,9 +1,6 @@
 import os
 import subprocess
 path=os.getcwd()
-for file in os.listdir(path):
-    filepath = path
-    print(filepath)
-    os.chdir(filepath)
-    strcmd = "ffmpeg -framerate 1 -pattern_type glob -i '*.jpg' -c:v libx264 -pix_fmt yuv420p out.mp4"
-    subprocess.call(strcmd,shell=True)
+
+strcmd = "ffmpeg -framerate 1 -pattern_type glob -i '*.jpg' -c:v libx264 -pix_fmt yuv420p out.mp4"
+subprocess.call(strcmd,shell=True)
